a30e9b6671af0b480fad3f20f602a2dc321ba15b
Михаил <misha13022008@gmail.com>
2026-07-06 02:36:15 +0300

    feat: add tests

---
+++ tests/tasks_tester.py
@@ -0,0 +1,44 @@
+import subprocess
+from pathlib import Path
+
+tasks = ["task1.1", "task1.2", "task1.3", "task1.4"]
+build = Path("build-tests")
+build.mkdir(exist_ok=True)
+
+for task in tasks:
+    subprocess.run([
+        "cmake", "-S", ".", "-B", build
+    ], check=True)
+
+    subprocess.run([
+        "cmake", "--build", build,
+    ], check=True)
+
+ok = 0
+total = 0
+
+for task in tasks:
+    for input_file in sorted(Path("tests", task).glob("*.in")):
+        output_file = input_file.with_suffix(".out")
+
+        actual = subprocess.run(
+            [str(build / task)],
+            input=input_file.read_text(),
+            text=True,
+            capture_output=True,
+        ).stdout
+
+        expected = output_file.read_text()
+        total += 1
+
+        if actual == expected:
+            print("OK", input_file)
+            ok += 1
+        else:
+            print("FAIL", input_file)
+            print("expected:")
+            print(repr(expected))
+            print("actual:")
+            print(repr(actual))
+
+print(f"{ok}/{total} passed")
